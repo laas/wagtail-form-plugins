a4b9dc4db88c724816ac1032122ed66f1e99e6c2
Nathanaël Jourdane <njourdane@laas.fr>
2024-09-06 17:23:17 +0200

    add conditional forms client-side code

---
+++ forms/static/forms/js/form.js
@@ -0,0 +1,60 @@
+const operators = {
+    'eq': ['=', (a, b) => a === b],
+    'neq': ['≠', (a, b) => a !== b],
+    'lt': ['<', (a, b) => parseFloat(a) < parseFloat(b)],
+    'lte': ['≤', (a, b) => parseFloat(a) <= parseFloat(b)],
+    'ut': ['>', (a, b) => parseFloat(a) > parseFloat(b)],
+    'ute': ['≥', (a, b) => parseFloat(a) >= parseFloat(b)],
+    'in': ['∈', (a, b) => b.includes(a)],
+}
+
+function compute_visibility_condition(vc) {
+    if (vc.field_id) {
+        const dom_field = document.getElementById(vc.field_id)
+        const [operator_str, operator_func] = operators[vc.operator]
+        return {
+            formula: `${ vc.field_label } ${ operator_str } "${ vc.value }"`,
+            str: `"${ dom_field.value }" ${ operator_str } "${ vc.value }"`,
+            result: operator_func(dom_field.value, vc.value),
+        }
+    }
+
+    if (vc.and) {
+        const computed_expr = vc.and.map((_vc) => compute_visibility_condition(_vc))
+        return {
+            formula: `(${ computed_expr.map((_vc) => _vc.formula).join(') AND (') })`,
+            str: `(${ computed_expr.map((_vc) => _vc.str).join(') AND (') })`,
+            result: computed_expr.every((_vc) => _vc.result),
+        }
+    }
+
+    if (vc.or) {
+        const computed_expr = vc.or.map((_vc) => compute_visibility_condition(_vc))
+        return {
+            formula: `(${ computed_expr.map((_vc) => _vc.formula).join(') OR (') })`,
+            str: `(${ computed_expr.map((_vc) => _vc.str).join(') OR (') })`,
+            result: computed_expr.some((_vc) => _vc.result),
+        }
+    }
+
+    return {formula: '∅', str: '∅', result: true}
+}
+
+function update_fields_visibility() {
+    console.clear()
+    for(const dom_field of document.getElementsByClassName('field-row')) {
+        const vc = JSON.parse(dom_field.getAttribute('data-vc'));
+        const cvc = compute_visibility_condition(vc)
+        console.log(`${vc.field_label}: ${ cvc.formula }  ⇒  ${ cvc.str }  ⇒  ${ cvc.result }`)
+
+        dom_field.style.display = cvc.result ? '' : 'none';
+        // dom_field.style.backgroundColor = cvc.result ? '' : 'lightGrey';
+    }
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    update_fields_visibility()
+    Array.from(document.querySelectorAll('.field-row input')).forEach((dom_input) => {
+        dom_input.addEventListener('change', () => update_fields_visibility())
+    });
+});
