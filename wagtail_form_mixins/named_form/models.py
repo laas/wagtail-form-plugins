42e4fb48bba344a98287edc21d3fc0bb9e14c834
Nathanaël Jourdane <njourdane@laas.fr>
2024-10-22 15:37:21 +0200

    nm: improve user display in submission list

--- wagtail_form_mixins/named_form/models.py
+++ wagtail_form_mixins/named_form/models.py
@@ -24,7 +24,15 @@ class NamedFormSubmission(AbstractFormSubmission):
 
 
 class NamedSubmissionsListView(SubmissionsListView):
-    pass
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        if not self.is_export:
+            for data_row in context["data_rows"]:
+                user = data_row["fields"][0]
+                data_row["fields"][0] = f"{user.first_name} {user.last_name}" if user else "-"
+
+        return context
 
 
 class NamedFormMixin(PluginBase):
