281f89c64ed181a1a44ef4eb4356aadf6c1a00c7
Nathanaël Jourdane <njourdane@laas.fr>
2024-11-08 11:47:50 +0100

    ex: remove unnecessary permission-related hooks

--- example/wagtail_hooks.py
+++ example/wagtail_hooks.py
@@ -1,4 +1,3 @@
-from django.contrib.auth.models import Permission
 from django.urls import reverse
 
 from wagtail import hooks
@@ -14,36 +13,6 @@ hooks.register("insert_global_admin_css", wfm_hooks.templating_admin_css)
 hooks.register("insert_global_admin_css", wfm_hooks.conditional_fields_admin_css)
 hooks.register("insert_global_admin_css", wfm_hooks.emails_admin_css)
 hooks.register("insert_global_admin_css", wfm_hooks.nav_buttons_admin_css)
-
-
-def permissions(app, model):
-    return Permission.objects.filter(
-        content_type__app_label=app,
-        codename__in=[f"view_{model}", f"add_{model}", f"change_{model}", f"delete_{model}"],
-    )
-
-
-@hooks.register("register_permissions")
-def formpage_permissions():
-    return permissions("example", "formpage")
-
-
-@hooks.register("register_permissions")
-def formindexpage_permissions():
-    return permissions("example", "formindexpage")
-
-
-@hooks.register("register_permissions")
-def formsubmission_permissions():
-    return permissions("example", "customformsubmission")
-
-
-@hooks.register("filter_form_submissions_for_user")
-def construct_forms_for_user(user, queryset):
-    if not user.is_superuser:
-        queryset = queryset.none()
-
-    return queryset
 
 
 @hooks.register("register_page_listing_buttons")
