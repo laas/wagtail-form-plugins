cc4d3866af27c1eafc59599fbbae2497844f1387
Nathanaël Jourdane <njourdane@laas.fr>
2024-12-21 18:31:25 +0100

    rename project

--- wagtail_form_mixins/nav_buttons/models.py
+++
@@ -1,9 +0,0 @@
-from wagtail_form_mixins.base.models import FormMixin
-
-
-class NavButtonsFormMixin(FormMixin):
-    def submissions_amount(self):
-        return self.get_submission_class().objects.filter(page=self).count()
-
-    class Meta:
-        abstract = True
