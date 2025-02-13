069c77b7d750050547dddb3a435aec1397409d14
Nathanaël Jourdane <njourdane@laas.fr>
2025-02-13 10:41:48 +0100

    er: do not update untouched file fields on edit

--- wagtail_form_plugins/editable/models.py
+++ wagtail_form_plugins/editable/models.py
@@ -1,9 +1,10 @@
-from django.forms.widgets import HiddenInput, TextInput
+from django.forms.widgets import HiddenInput, TextInput, FileInput
 from django.forms.fields import CharField
 from django.contrib.auth import get_user_model
 from django.shortcuts import get_object_or_404, redirect
 from django.template.response import TemplateResponse
 from django.urls import reverse
+from wagtail.contrib.forms.utils import get_field_clean_name
 
 from wagtail_form_plugins.base.models import FormMixin
 
@@ -17,9 +18,19 @@ class EditableFormMixin(FormMixin):
                 submission_id = int(request.POST["edit"])
                 submission = get_object_or_404(self.get_submission_class(), pk=submission_id)
                 form = self.get_form(request.POST, request.FILES, page=self, user=request.user)
+                file_fields = [
+                    get_field_clean_name(field.label)
+                    for field in form.fields.values()
+                    if isinstance(field.widget, FileInput)
+                ]
 
                 if form.is_valid():
-                    for attr_key, attr_value in self.get_submission_attributes(form).items():
+                    attrs = self.get_submission_attributes(form)
+                    attrs["form_data"] = {
+                        k: v if k not in file_fields or v else submission.form_data[k]
+                        for k, v in attrs["form_data"].items()
+                    }
+                    for attr_key, attr_value in attrs.items():
                         setattr(submission, attr_key, attr_value)
                     submission.save()
                     redirect_args = {"page_id": self.pk}
