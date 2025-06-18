c0613808dc12b708b6efcf4328499db99a4c0b0e
Nathanaël Jourdane <njourdane@laas.fr>
2025-06-18 15:10:33 +0200

    format in disabled field in form submission, fiwing templating issue in emails and submission results

--- wagtail_form_plugins/templating/models.py
+++ wagtail_form_plugins/templating/models.py
@@ -1,7 +1,9 @@
 """Models definition for the Templating form plugin."""
 
+from typing import Any
 from django.http import HttpRequest, HttpResponseRedirect
 from wagtail_form_plugins.base.models import FormMixin
+from wagtail.contrib.forms.utils import get_field_clean_name
 
 from .formatter import TemplatingFormatter
 
@@ -10,6 +12,30 @@ class TemplatingFormMixin(FormMixin):
     """Form mixin for the Templating plugin. Used to format initial values and submissions."""
 
     formatter_class = TemplatingFormatter
+
+    def format_submission(self, context_data: dict[str, Any], formatter: TemplatingFormatter):
+        """Format the submission passed to the given context data, using the given formatter."""
+        form_submission = context_data["form_submission"]
+
+        disabled_fields = [
+            get_field_clean_name(field.value["label"])
+            for field in context_data["page"].form_fields
+            if field.value.get("disabled")
+        ]
+
+        new_submission_data = {}
+        for data_key, data_value in form_submission.form_data.items():
+            if data_key in disabled_fields:
+                fmt_data = formatter.format(data_value)
+                if fmt_data != data_value:
+                    new_submission_data[data_key] = fmt_data
+
+        if new_submission_data:
+            form_submission.form_data = {
+                **form_submission.form_data,
+                **new_submission_data,
+            }
+            form_submission.save()
 
     def serve(self, request: HttpRequest, *args, **kwargs):
         """Serve the form page."""
@@ -24,7 +50,10 @@ class TemplatingFormMixin(FormMixin):
                 if field.initial:
                     field.initial = formatter.format(field.initial)
 
-        if "form_submission" in response.context_data:
+        else:
+            self.format_submission(response.context_data, formatter)
+            formatter = self.formatter_class(response.context_data)
+
             for email in response.context_data["page"].emails_to_send:
                 for field_name in ["subject", "message", "recipient_list", "reply_to"]:
                     email.value[field_name] = formatter.format(str(email.value[field_name]))
