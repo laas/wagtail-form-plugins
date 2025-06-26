b724885d11de37e6b5aafc7123e16af8a469b093
Nathanaël Jourdane <njourdane@laas.fr>
2025-06-26 10:55:10 +0200

    add wfp fields in forms module

--- wagtail_form_plugins/forms.py
+++ wagtail_form_plugins/forms.py
@@ -1,7 +1,18 @@
 """Import all form builder classes of form plugins."""
 # ruff: noqa: F401
 
-from .streamfield.forms import StreamFieldFormBuilder
+from .streamfield.forms import (
+    StreamFieldFormBuilder,
+    CharField,
+    DateField,
+    DateTimeField,
+    EmailField,
+    URLField,
+    DecimalField,
+    BooleanField,
+    ChoiceField,
+    MultipleChoiceField,
+)
 from .file_input.forms import FileInputFormBuilder
 from .datepickers.forms import DatePickersFormBuilder
 from .label.forms import LabelFormBuilder
