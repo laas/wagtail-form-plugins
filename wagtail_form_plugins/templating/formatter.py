48f8fc16f5f27e01dbb6a992159894e416dcdc85
Nathanaël Jourdane <njourdane@laas.fr>
2025-08-28 11:15:58 +0200

    tp: dont show star prefix in field choices

--- wagtail_form_plugins/templating/formatter.py
+++ wagtail_form_plugins/templating/formatter.py
@@ -78,7 +78,7 @@ class TemplatingFormatter:
             if field.block.name in ["checkboxes", "dropdown", "multiselect", "radio"]:
                 choices = {get_field_clean_name(c): c for c in field.value["choices"].splitlines()}
                 if fmt_value:
-                    fmt_value = ", ".join([choices[v] for v in fmt_value.split(",")])
+                    fmt_value = ", ".join([choices[v].lstrip("*") for v in fmt_value.split(",")])
 
             fields[field_id] = (field.value["label"], fmt_value)
         return fields
