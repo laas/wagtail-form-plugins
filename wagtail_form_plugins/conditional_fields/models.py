29565ac529de5fca57a16dc07b4669b6e4cefe84
Nathanaël Jourdane <njourdane@laas.fr>
2025-09-04 14:02:59 +0200

    fix potential key error

--- wagtail_form_plugins/conditional_fields/models.py
+++ wagtail_form_plugins/conditional_fields/models.py
@@ -138,7 +138,7 @@ class ConditionalFieldsFormMixin(FormMixin):
                 results = [process_rule(sub_rule) for sub_rule in rule["rules"]]
                 return all(results) if rule["field"] == "and" else any(results)
 
-            a = form_data.get(slugs[rule["field"]])
+            a = form_data.get(slugs.get(rule["field"]))
             field_type = field_types[rule["field"]]
 
             if field_type in ["singleline", "multiline", "email", "hidden", "url"]:
