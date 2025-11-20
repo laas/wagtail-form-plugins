5cd8c6dccb01ba3786777d4da13d676b0aaed0d1
Nathanaël Jourdane <njourdane@laas.fr>
2025-11-20 16:09:02 +0100

    move submission-related context modification to Edit plugin submission view

--- wagtail_form_plugins/plugins/editable/__init__.py
+++ wagtail_form_plugins/plugins/editable/__init__.py
@@ -3,12 +3,14 @@
 from wagtail_form_plugins.streamfield.plugin import Plugin
 
 from .models import EditableFormPage
+from .views import StreamFieldSubmissionsListView
 
 
 class Editable(Plugin):
     """Editable plugin: allow form admins to modify form submissions."""
 
     form_page_class = EditableFormPage
+    submission_list_view_class = StreamFieldSubmissionsListView
 
 
 __all__ = [
--- wagtail_form_plugins/plugins/editable/views.py
+++ wagtail_form_plugins/plugins/editable/views.py
@@ -22,25 +22,10 @@ class StreamFieldSubmissionsListView(SubmissionsListView):
         ctx_data = super().get_context_data(**kwargs)
 
         submissions: dict[str, FormSubmission] = {sub.id: sub for sub in ctx_data["submissions"]}
-        header: list[str] = [head["name"] for head in ctx_data["data_headings"]]
-        fields = self.form_page.get_form_fields_dict()
+        ctx_data["data_headings"].append({"name": "edit_button", "label": "Edit", "order": None})
 
         for row_idx, row in enumerate(ctx_data["data_rows"]):
             submission = submissions[row["model_id"]]
-            for col_idx, col_value in enumerate(row["fields"]):
-                field_header = header[col_idx]
-                if field_header in fields:
-                    fmt_value = self.form_page.format_field_value(
-                        fields[field_header],
-                        submission.form_data.get(field_header, None),
-                        in_html=True,
-                    )
-                elif field_header == "submit_time":
-                    fmt_value = col_value.strftime("%d/%m/%Y, %H:%M")
-                else:
-                    fmt_value = col_value
-
-                ctx_data["data_rows"][row_idx]["fields"][col_idx] = fmt_value or "-"
 
             link_html = format_html(
                 '<a class="w-header-button button" href="{url}?edit={submission_id}">edit</a>',
